8d166bd551b4a128a5b23968756f2ae7db505850
AjayRahul1 <p.ajayrahul02@gmail.com>
2023-08-09 16:59:19 +0530

    version 0.0.9 alpha
    1. Able to get the text entered from the website.
    2. Print the entered text in summary
    3. HTMX code worked in form tag.
    4. Responsive website for Desktop and Mobile

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from doc_summariser_functions import take_input_paragraph
 
 app = Flask(__name__)
 
 @app.get("/")
 def summarise_page():
   return render_template('index.html')
+
+@app.route("/summarise_txt")
+def summarise_the_txt():
+  input_essay = request.args.get('input_for_summarise')
+  print(input_essay)
+  return f"""<p class="fs-3 fw-bold">Summary</p>\n<p class="lh-sm"> {input_essay} </p>"""
